3ab63a8fd6fb116cccb8331252a9c94d4f7321d1
Fenet Firomsa <fenetfiromsa2012@gmail.com>
2025-10-26 18:32:51 +0300

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,22 +1,16 @@
-import { Toaster } from "react-hot-toast";
+import { Toaster } from 'react-hot-toast';
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // <-- import BrowserRouter
 import App from "./App.jsx";
 import "./index.css";
 import { CartProvider } from "./context/CartContext.jsx";
-import { AuthProvider } from "./context/AuthContext.jsx"; // <-- import AuthProvider
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter> {/* <-- wrap everything in BrowserRouter */}
-      <AuthProvider> {/* <-- now AuthProvider can safely use useNavigate */}
-        <CartProvider>
-          <App />
-          <Toaster position="top-right" />
-        </CartProvider>
-      </AuthProvider>
-    </BrowserRouter>
+    <CartProvider>
+      <App />
+      <Toaster position="top-right" />
+    </CartProvider>
+     
   </React.StrictMode>
 );
-
