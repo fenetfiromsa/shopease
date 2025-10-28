060511f81c247146c7bc8ee0a0f16723fe2c0fd5
Fenet Firomsa <fenetfiromsa2012@gmail.com>
2025-10-28 05:06:30 +0300

    Update axios.js

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -5,7 +5,7 @@ const baseURL = import.meta.env.VITE_API_URL || 'http://localhost:5001';
 
 const axiosInstance = axios.create({
   baseURL,
-  withCredentials: true, // send cookies if needed
+  withCredentials: true, 
   headers: {
     'Content-Type': 'application/json',
   },
