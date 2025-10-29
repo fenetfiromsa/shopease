39d60167b5191713462aa7deaa2260fb58f15f07
Fenet Firomsa <fenetfiromsa2012@gmail.com>
2025-10-29 14:27:07 +0300

    Update productRoutes.js

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,20 +1,32 @@
+
 const express = require('express');
+
+const multer = require("multer");
 const router = express.Router();
 const {
+  createProduct,
   getProducts,
   getProductById,
-  createProduct,
   updateProduct,
   deleteProduct,
 } = require('../controllers/productController');
+
+
 const { protect, admin } = require('../middleware/auth');
 
 
+
+const storage = multer.memoryStorage(); 
+const upload = multer({ storage });
 router.get('/', getProducts);
+
+
 router.get('/:id', getProductById);
 
-router.post('/', protect, admin, createProduct);
-router.put('/:id', protect, admin, updateProduct);
-router.delete('/:id', protect, admin, deleteProduct);
+
+router.post("/", protect, admin, upload.single("image"), createProduct);
+router.put("/:id", protect, admin, upload.single("image"), updateProduct);
+router.delete("/:id", protect, admin, deleteProduct);
 
 module.exports = router;
+   
